0f3a7789e135d37b405c7ca24f2dffb23da45949
gooners1886 <gooners1886@163.com>
2016-05-04 22:47:27 +0800

    CreateFolderAndSubfolder  模块 提交
    CreateFolderAndSubfolder  模块 提交

---
+++ CreateFolderAndSubfolder/CreateFolderAndSubfolder.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <string>
+// 1. 可以创建文件夹和子文件夹
+// 2. 可以输入相对路径 或 绝对路径
+// 3. 如果路径存在，就不创建；如果不存在，就创建
+void myMakeDirectory( std::string strPath )
+{
+	std::string strCmd;
+	//相对路径   .\\yy1\\\yy2
+	//绝对路径   E:\\aaa\\bbb
+	strCmd = "md " + strPath; // md .\\yy1\\\yy2         md E:\\aaa\\bbb
+	system( strCmd.c_str() );
+
+	return;
+}
+
+
+void main()
+{
+	//相对路径：
+	std::string test_path1 = ".\\yy1\\yy2";
+	//绝对路径：
+	std::string test_path2 = "E:\\ccc\\ddd";
+
+	myMakeDirectory( test_path1 );
+	myMakeDirectory( test_path2 );
+	
+}
