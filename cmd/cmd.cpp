012df6bb06cbdc3dd49bafee1d182c8eaff43f0e
gooners1886 <gooners1886@163.com>
2016-08-30 11:37:27 +0800

    cmd 模块新创建
    cmd 模块新创建

---
+++ cmd/cmd.cpp
@@ -0,0 +1,39 @@
+﻿#include <string>
+
+using namespace std;
+
+
+// 文件复制
+void cmdCopyFile( string strCpySrcPath, string strCpyDstFolder )
+{
+	string strCopyCmd = "copy " + strCpySrcPath + " " + strCpyDstFolder;
+	system(strCopyCmd.c_str());
+
+	return;
+}
+
+// 文件剪切
+void cmdMoveFile( string strMoveSrcPath, string strCpyDstFolder )
+{
+	string strMoveCmd = "move " + strMoveSrcPath + " " + strCpyDstFolder;
+	system(strMoveCmd.c_str());
+
+	return;
+}
+
+int main()
+{
+	// 文件复制
+	string strCpySrcPath = ".\\CpySrcFolder\\copysrc.txt";
+	string strCpyDstFolder = ".\\CpyDstFolder\\";
+	cmdCopyFile( strCpySrcPath, strCpyDstFolder );
+
+	// 文件剪切
+	string strMoveSrcPath = ".\\MoveSrcFolder\\movesrc.txt";
+	string strMoveDstFolder = ".\\MoveDstFolder\\";
+	cmdMoveFile( strMoveSrcPath, strMoveDstFolder );
+
+
+
+	return 1;
+}
